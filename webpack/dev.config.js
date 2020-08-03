01fabf12ebcb1b46dd0c2987074b0e5d71b932c5
Alexandre <alexandreargm@gmail.com>
2020-08-03 14:08:34 +0200

    added brosersync support

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -1,6 +1,13 @@
 const baseConfig = require('./base.config.js');
 const merge = require('webpack-merge');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 
 module.exports = merge(baseConfig, {
   devtool: 'eval-source-map',
+  plugins: [
+    new BrowserSyncPlugin({
+      files: ['**/*.php', 'style.css'],
+      proxy: 'http://wordpress.ddev.site'
+    })
+  ],
 });
